7364afd747b7f27e77910cbed86989c423925fcb
AlinaRoth <malina.root@mail.ru>
2017-01-13 21:07:24 +0700

    fixes

--- lab4.cs
+++ lab4.cs
@@ -9,29 +9,47 @@ namespace ConsoleApplication1
         public int Recipient { get; set; }
     }
 
+    public class CustomThread
+    {
+        public int Number { get; set; }
+        public CustomThread Next { get; set; }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            CustomThread[] threads = new CustomThread[51];
+            threads[50] = new CustomThread();
+            threads[50].Next = null
+            threads[50].Number = 50;
+            
+            for (int i = 49; i >= 1; i--)
+            {
+                threads[i] = new CustomThread();
+                threads[i].Next = threads[i + 1];
+                threads[i].Number = i - 1;
+            }
+
             var token = new Token();
             token.Data = "I will show only to chosen one";
-            token.Recipient = 24;
+            token.Recipient = 23;
 
-            Thread thread = new Thread(delegate () { Func(1, token); });
+            Thread thread = new Thread(delegate () { Func(threads[1], token); });
             thread.Start();
         }
 
-        static void Func(int id, Token token)
+        static void Func(CustomThread th, Token token)
         {
-            if (id == token.Recipient)
+            if (th.Number == token.Recipient)
             {
-                Console.WriteLine(token.Data + " (" + id.ToString() + ")");
+                Console.WriteLine(token.Data + " (" + th.Number.ToString() + ")");
                 Console.Read(); //Приостановим основной поток
             }
             else
             {
                 Console.WriteLine("Passed further");
-                Thread thread = new Thread(delegate () { Func(id + 1, token); });
+                Thread thread = new Thread(delegate () { Func(th.Next, token); });
                 thread.Start();
             }
         }
--- lab4.go
+++ lab4.go
@@ -1,30 +1,46 @@
 package main
+
 import (
 	"fmt"
-    	"strconv"
+	"strconv"
 )
 
 type Token struct {
-	data string
+	data      string
 	recipient int
 }
 
-func thread(id int, token Token, c chan string) {
-	if (token.recipient == id) {
-		response := token.data + " (" + strconv.Itoa(id) + ")"
+type CustomThread struct {
+	number int
+	next   *CustomThread
+}
+
+func thread(th *CustomThread, token Token, c chan string) {
+	fmt.Println(th.number)
+	if token.recipient == th.number {
+		response := token.data + " (" + strconv.Itoa(th.number) + ")"
 		c <- response
+		close (c)
 	} else {
 		response := "Passed further"
 		c <- response
-		go thread(id+1, token, c)
+		go thread(th.next, token, c)
 	}
 }
 
 func main() {
-	channel := make(chan string);
-	recipient := 24;
- 	go thread(1, Token{data: "I only show for chosen one", recipient: recipient}, channel)	
-	for i := 0; i < recipient-1; i++ {
+	// Создаем наши потоки
+	var threads [51]CustomThread
+	
+	threads[50] = CustomThread{number: 50}
+	for i := 49; i >= 1; i-- {
+		threads[i] = CustomThread{number: i-1, next: &threads[i+1]}
+	}
+	
+	fmt.Println(threads)
+	channel := make(chan string)
+	go thread(&threads[1], Token{data: "I only show for chosen one", recipient: 23}, channel)
+	for i := 0; i < 50; i++ {
 		fmt.Println(<-channel)
 	}
 	fmt.Println(<-channel)
